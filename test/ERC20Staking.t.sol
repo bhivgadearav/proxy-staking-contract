a62bf35c21db8b2282903110bfdf0fd405974dda
Arav Bhivgade <hazeifygameplay5927@gmail.com>
2025-01-26 11:48:31 +0530

    redeem rewards for erc20 staking passes tests
    written tests for redeem rewards function in erc20 staking contract

--- test/ERC20Staking.t.sol
+++ test/ERC20Staking.t.sol
@@ -19,6 +19,7 @@ contract ETHStakingTest is Test {
         stakingLogicContract = new ERC20StakingLogic();
         stakingDataContract = new ERC20StakingData(address(stakingLogicContract));
         stakeTokenContract = new StakeToken(address(user), address(stakingDataContract));
+        stakingDataContract.setStakeToken(address(stakeTokenContract));
     }
 
     function testStake() public {
@@ -96,6 +97,58 @@ contract ETHStakingTest is Test {
 
         uint256 rewards = abi.decode(data, (uint256));
         assertEq(rewards, 10);
+
+        vm.stopPrank();
+    }
+
+    function testRedeemRewards() public {
+        bool success;
+        bytes memory data;
+        vm.deal(user, 10 ether);
+        vm.startPrank(user);
+
+        customTokenContract = new CustomToken();
+        stakingDataContract.setValidToken(address(customTokenContract));
+
+        customTokenContract.mint(address(user), 10**19);
+
+        customTokenContract.approve(address(stakingDataContract), 10**19);
+
+        (success, ) = address(stakingDataContract).call(abi.encodeWithSignature("stake(address,uint256)", address(customTokenContract), 10**19));
+        assertTrue(success);
+
+        (uint256 amount, ) = stakingDataContract.stakers(user);
+        assertEq(amount, 10**19);
+
+        vm.warp(block.timestamp + 1 days);
+
+        (success, data) = address(stakingDataContract).call(abi.encodeWithSignature("getRewards()"));
+        assertTrue(success);
+        assertTrue(data.length == 32, "Return data should be 32 bytes (uint256)");
+
+        uint256 rewards = abi.decode(data, (uint256));
+        assertEq(rewards, 10);
+
+        (success, ) = address(stakingDataContract).call(abi.encodeWithSignature("redeemRewards()"));
+        assertTrue(success);
+
+        uint256 timeTillRedeem = stakingDataContract.rewardClaims(user);
+        assertNotEq(timeTillRedeem, 0);
+
+        (success, data) = address(stakingDataContract).call(abi.encodeWithSignature("getRewards()"));
+        assertTrue(success);
+        assertTrue(data.length == 32, "Return data should be 32 bytes (uint256)");
+
+        rewards = abi.decode(data, (uint256));
+        assertEq(rewards, 10);
+
+        vm.warp(block.timestamp + 22 days);
+
+        (success, ) = address(stakingDataContract).call(abi.encodeWithSignature("redeemRewards()"));
+        assertTrue(success);
+
+        uint256 userTokenBalance = stakeTokenContract.balanceOf(user);
+        assertEq(userTokenBalance, 10);
 
         vm.stopPrank();
     }
